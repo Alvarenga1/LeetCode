4c7a31f880d1f299feaae39c4a28f805d2118be4
Renan Alvarenga <renan.alvarenga@wisetechglobal.com>
2023-07-06 15:22:52 +1000

    Implemented 2652

---
+++ 2652/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Solution2652;
+public class Solution
+{
+    public int SumOfMultiples(int n)
+    {
+        int result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+            {
+                result += i;
+            }
+        }
+        return result;
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution2652.Solution();
+
+            var result = solution.SumOfMultiples(7);
+
+            Console.WriteLine(result);
+        }
+    }
+}
