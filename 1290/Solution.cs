4d3bfbcafa37e4112a0e9c93bd94c312c711ebb8
Renan Alvarenga <renan.alvarenga@wisetechglobal.com>
2023-07-13 11:32:38 +1000

    Implemented 1290

---
+++ 1290/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace _1290;
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public int GetDecimalValue(ListNode head)
+    {
+        int val = 0;
+        string binary = "";
+
+        while (head != null)
+        {
+            binary = String.Concat(binary, head.val);
+            head = head.next;
+        }
+
+        for (int i = 0; i < binary.Length; i++)
+        {
+            if (binary[binary.Length - i - 1] == '1')
+            {
+                val += (int)Math.Pow(2, i);
+            }
+        }
+
+
+        return val;
+    }
+}
