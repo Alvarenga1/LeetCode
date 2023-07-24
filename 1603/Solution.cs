6383df759f7d9b3788036578c673404122a7f1a1
Renan Alvarenga <renan.alvarenga@wisetechglobal.com>
2023-07-24 16:32:10 +1000

    Implemented 1603

---
+++ 1603/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace _1603;
+public class ParkingSystem
+{
+    int[] carSpaces = new int[3];
+
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        this.carSpaces[0] = big;
+        this.carSpaces[1] = medium;
+        this.carSpaces[2] = small;
+    }
+
+    public bool AddCar(int carType)
+    {
+        if (carSpaces[carType - 1] > 0)
+        {
+            carSpaces[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,9 +4,12 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var solution = new _771.Solution();
+        var solution = new _1603.ParkingSystem(1, 1, 0);
 
-        var result = solution.NumJewelsInStones("aA", "aAAbbbb");
+        var result = solution.AddCar(1);
+        solution.AddCar(2);
+        solution.AddCar(3);
+        solution.AddCar(1);
 
         Console.WriteLine(result);
     }
